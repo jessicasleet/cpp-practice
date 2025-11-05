59f6354074acc98e39ae1ce0c3f12e64feb5163c
jessicasleet <jks73uni@gmail.com>
2025-11-05 16:18:02 +0000

    Add testing.cpp - first C++ program

---
+++ print("hello world").py
@@ -0,0 +1,4 @@
+print("hello world")
+
+for n in range(3):
+    print('LOL')
---
+++ testing.cpp
@@ -0,0 +1,7 @@
+#include <iostream>   //library that allows input + output
+using namespace std;  //so i dont have to prefix everything with std::
+
+int main() {
+    cout << "Hello testing testings!!!" << endl;
+    return 0;
+}
